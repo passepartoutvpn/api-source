11ef494add2637fdc23c044d35c45cc31bfd68c0
Davide De Rosa <keeshux@gmail.com>
2022-10-30 14:50:09 +0100

    Infer providers from latest commit

--- ci/update-providers.sh
+++ ci/update-providers.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-./generate.sh --providers "$*"
+if [[ -n $* ]]; then
+    PROVIDERS="$*"
+else
+    PROVIDERS=""
+    for file in $CHANGED_FILES; do
+        CHANGED_PROVIDER=`echo $file | sed -E "s/providers\/([A-z]+)\/.+/\1/g"`
+        PROVIDERS="$PROVIDERS,$CHANGED_PROVIDER"
+    done
+fi
+./generate.sh --providers "$PROVIDERS"

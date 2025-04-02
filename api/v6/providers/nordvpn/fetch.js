3adcf9f9906ed0de41489e7342045db107432625
Davide <keeshux@gmail.com>
2025-04-02 15:26:04 +0200

    Propagate cache

--- api/v6/providers/nordvpn/fetch.js
+++ api/v6/providers/nordvpn/fetch.js
@@ -103,7 +103,7 @@ function getInfrastructure() {
         response: {
             presets: presets,
             servers: servers,
-            lastUpdate: json.lastModified
+            cache: json.cache
         }
     };
 }
